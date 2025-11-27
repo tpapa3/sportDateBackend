c4f337b9b0143cdbf0dcd266ed29d2405e0c04ff
tpapa3 <thanasispapadimitriou3@gmail.com>
2025-11-27 15:32:25 +0200

    upgrade version of spring boot to avoid vuln in packages

--- src/main/java/bookCourt/sport/SportApplication.java
+++ src/main/java/bookCourt/sport/SportApplication.java
@@ -2,7 +2,8 @@ package bookCourt.sport;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
+
 
 @SpringBootApplication
 @EntityScan(basePackages = "bookCourt.sport.*")
